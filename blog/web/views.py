7932a0167a276e08c072e4fb6fbe888cd335093d
594617357@qq.com <594617357@qq.cm>
2019-04-27 19:01:22 +0800

    增加了新功能

--- blog/web/views.py
+++ blog/web/views.py
@@ -1,20 +1,40 @@
 from flask import Blueprint, render_template, request
 
 from back.models import Article, User, Category, Link
+import math
 
 web=Blueprint('web',__name__)
 
 @web.route('/index/',methods=["GET"])
 def index():
 	#获取服务器里所有的文章列表
+	all_article=len(Article.query.filter().all())
+	count_page=math.ceil(len(Article.query.filter().all())/5)+1
 	article_list=Article.query.filter().order_by(-Article.id).limit(5).all()
-	# print(article_list)
 	user=User.query.filter(User.username=="袁满潭").first()
 	category_list=Category.query.filter().order_by(Category.id).all()
 	link_list=Link.query.filter().order_by(Link.id).all()
-	# print(link_list)
+
 	#将数据传入模板并渲染
-	return  render_template('web/index.html',title="最新发布",article_list=article_list,user=user,category_list=category_list,link_list=link_list)
+	return  render_template('web/index.html',title="最新发布",article_list=article_list,user=user,category_list=category_list,link_list=link_list,count_page=count_page,all_article=all_article)
+
+@web.route('/index/<int:page>',methods=["GET"])
+def get_article(page):
+		all_article = len(Article.query.filter().all())
+		count_page = math.ceil(len(Article.query.filter().all()) / 5) + 1
+		article_list = Article.query.filter().order_by(-Article.id).offset((page - 1) * 5).limit(5).all()
+		user = User.query.filter(User.username == "袁满潭").first()
+		category_list = Category.query.filter().order_by(Category.id).all()
+		link_list = Link.query.filter().order_by(Link.id).all()
+
+		# 将数据传入模板并渲染
+		return render_template('web/index.html', title="最新发布", article_list=article_list, user=user,
+	category_list=category_list, link_list=link_list, count_page=count_page,all_article=all_article,page_now=page)
+
+
+
+
+
 
 # @web.route('/article/',methods=["GET"])
 # def article():
@@ -37,15 +57,34 @@ def about():
 	return render_template('web/about.html',category_list=category_list,link_list=link_list)
 
 
-@web.route('/category/<string:category>')
-def category(category):
+# @web.route('/category/<string:category>')
+# def category(category):
+# 	categ=Category.query.filter(Category.c_name==category).first()
+# 	article_list=categ.arts
+# 	article_list.reverse()
+# 	user = User.query.filter(User.username == "袁满潭").first()
+# 	category_list = Category.query.filter().order_by(Category.id).all()
+# 	title=category
+# 	link_list = Link.query.filter().order_by(Link.id).all()
+# 	return render_template('web/category.html',article_list=article_list,user=user,category_list=category_list,title=title,link_list=link_list)
+
+
+
+@web.route('/<string:category>/<int:page>')
+def category(category,page):
 	categ=Category.query.filter(Category.c_name==category).first()
-	article_list=categ.arts
-	article_list.reverse()
+	#获取该分类的所有文章
+	all_article=len(categ.arts)
+	id=categ.id
+	# print(id)
+	article_list=Article.query.filter(Article.category_id==id).offset((page - 1) * 5).limit(5).all()
+	# article_list.reverse()
 	user = User.query.filter(User.username == "袁满潭").first()
 	category_list = Category.query.filter().order_by(Category.id).all()
 	title=category
 	link_list = Link.query.filter().order_by(Link.id).all()
-	return render_template('web/category.html',article_list=article_list,user=user,category_list=category_list,title=title,link_list=link_list)
+	count_page = math.ceil(all_article/ 5) + 1
+	# article_list = Article.query.filter().order_by(-Article.id).offset((page - 1) * 5).limit(5).all()
+	return render_template('web/category.html',article_list=article_list,user=user,category_list=category_list,title=title,link_list=link_list,count_page=count_page,page_now=page,all_article=all_article)
 
 
